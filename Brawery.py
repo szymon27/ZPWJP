d57dde672bb21216ade8f2a1f9007a4e84d3a5c1
szymon27 <szymonsamsel27@gmail.com>
2024-04-20 19:05:03 +0200

    importy

---
+++ Brawery.py
@@ -0,0 +1,42 @@
+class Brawery:
+    def __init__(self, id: str, name: str, brewery_type: str, address_1: str,
+                 address_2: str, address_3: str, city: str,
+                 state_province: str, postal_code: str, country: str,
+                 longitude: str, latitude: str, phone: str, website_url: str,
+                 state: str, street: str):
+        self.id = id
+        self.name = name
+        self.brewery_type = brewery_type
+        self.address_1 = address_1
+        self.address_2 = address_2
+        self.address_3 = address_3
+        self.city = city
+        self.state_province = state_province
+        self.postal_code = postal_code
+        self.country = country
+        self.longitude = longitude
+        self.latitude = latitude
+        self.phone = phone
+        self.website_url = website_url
+        self.state = state
+        self.street = street
+
+    id: str
+    name: str
+    brewery_type: str
+    address_1: str
+    address_2: str
+    address_3: str
+    city: str
+    state_province: str
+    postal_code: str
+    country: str
+    longitude: str
+    latitude: str
+    phone: str
+    website_url: str
+    state: str
+    street: str
+
+    def __str__(self):
+        return f'{self.id} {self.name} {self.brewery_type}'
--- zad_8.py
+++ zad_8.py
@@ -1,50 +1,7 @@
 import requests
 import json
 import argparse
-
-
-class Brawery:
-    def __init__(self,  id: str, name: str, brewery_type: str, address_1: str,
-                 address_2: str, address_3: str, city: str,
-                 state_province: str, postal_code: str, country: str,
-                 longitude: str, latitude: str, phone: str, website_url: str,
-                 state: str, street: str):
-        self.id = id
-        self.name = name
-        self.brewery_type = brewery_type
-        self.address_1 = address_1
-        self.address_2 = address_2
-        self.address_3 = address_3
-        self.city = city
-        self.state_province = state_province
-        self.postal_code = postal_code
-        self.country = country
-        self.longitude = longitude
-        self.latitude = latitude
-        self.phone = phone
-        self.website_url = website_url
-        self.state = state
-        self.street = street
-
-    id: str
-    name: str
-    brewery_type: str
-    address_1: str
-    address_2: str
-    address_3: str
-    city: str
-    state_province: str
-    postal_code: str
-    country: str
-    longitude: str
-    latitude: str
-    phone: str
-    website_url: str
-    state: str
-    street: str
-
-    def __str__(self):
-        return f'Id: {self.id}\nName: {self.name}\nType: {self.brewery_type}\nCity: {self.city}'
+from Brawery import Brawery
 
 
 if __name__ == '__main__':
