84ae995100ecff34987a519924f726a7d7cfd318
szymon27 <szymonsamsel27@gmail.com>
2024-04-25 23:04:44 +0200

    comment showing image

--- person_detector.py
+++ person_detector.py
@@ -8,11 +8,11 @@ def person_count(file_path):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     boxes, weights = hog.detectMultiScale(gray, winStride=(8, 8))
 
-    import numpy as np
-    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-    for (xA, yA, xB, yB) in boxes:
-        cv2.rectangle(img, (xA, yA), (xB, yB), (0, 255, 0), 2)
-    cv2.imshow('img', img)
-    cv2.waitKey()
+    # import numpy as np
+    # boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+    # for (xA, yA, xB, yB) in boxes:
+    #     cv2.rectangle(img, (xA, yA), (xB, yB), (0, 255, 0), 2)
+    # cv2.imshow('img', img)
+    # cv2.waitKey()
 
     return (len(boxes))
